0dc950ddaf5db1417a9f17c20e671fb8f5ce9050
gcychoi <gcychoi@gmail.com>
2025-05-08 14:06:42 -0400

    Added lab1a

---
+++ lab1a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+
+# Name   : lab1a.py
+# Author : Chung Yin Choi
+# Date   : May 8, 2025
+
+print('Hello world')
