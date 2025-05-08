ea92f8a68c7685890e9b311c9a4f0914a96b0fa2
gcychoi <gcychoi@gmail.com>
2025-05-08 14:46:31 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program will output "How old are you Isaac?" to the screen.'''
+
+
+# Name   : lab1b.py
+# Author : Chung Yin Choi
+# Date   : May 8, 2025
+
+name = 'Isaac'
+print('How old are you ' + name + '?')
