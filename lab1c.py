ded75b6d61b35b799fac3d8582ca1ebbed36d009
Chung Yin Choi <cychoi@myseneca.ca>
2025-05-10 21:17:52 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Description: This program will output "Isaac is 72 years old!" to the screen.'''
+
+
+# Name   : lab1c.py
+# Author : Chung Yin Choi
+# Date   : May 10, 2025
+
+
+name = 'Isaac'
+age = 72
+print('Isaac is ' + str(age) + ' years old!')
