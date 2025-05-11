71820978ef1a2bc0cc6ca37dd74198168513698d
Chung Yin Choi <cychoi@myseneca.ca>
2025-05-10 21:18:28 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Description: This program will output "x + y * z = result" to the screen.'''
+
+
+# Name   : lab1d.py
+# Author : Chung Yin Choi
+# Date   : May 10, 2025
+
+
+x = 10
+y = 2
+z = 5
+result = (x + y * z)
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
